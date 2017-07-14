133d98edcce98f2bcb2160b9cb6850d7c20e32d0
Ruben Fiszel <ruben.fiszel@epfl.ch>
2017-07-13 23:38:33 -0700

    before peek node

--- src/ParticleFilter.scala
+++ src/ParticleFilter.scala
@@ -6,118 +6,232 @@ import spatial.interpreter.Streams
 
 trait ParticleFilter extends SpatialStream {
 
-  type SReal = FltPt[_32,_0]
+  val N: scala.Int                                                    = 10
+  val initV: (scala.Double, scala.Double, scala.Double)               = (0.0, 0.0, 0.0)
+  val initP: (scala.Double, scala.Double, scala.Double)               = (0.0, 0.0, 0.0)
+  val initQ: (scala.Double, scala.Double, scala.Double, scala.Double) = (1.0, 0.0, 0.0, 0.0)
+  val initTime: scala.Double                                          = 0.0
+  val covGyro: scala.Double = 1.0
 
-  @struct case class SQuaternion(r: SReal, i: SReal, j: SReal, k: SReal)
-  @struct case class SVec3(x: SReal, y: SReal, z: SReal)
-  @struct case class SState(v: SVec3, p: SVec3)
-  @struct case class SParticle(w: SReal, q: SQuaternion, st: SState)  
+  type Real         = FltPt[_32, _0]
+  type Time         = Real
+  type Position     = Vec3
+  type Velocity     = Vec3
+  type Acceleration = Vec3
+  type Omega        = Vec3
+  type Attitude     = Quat
 
 
-  val N: scala.Int = 10
+  @struct case class Quat(r: Real, i: Real, j: Real, k: Real)
+  @struct case class Vec3(x: Real, y: Real, z: Real)
+  @struct case class IMU(t: Time, a: Acceleration, g: Omega)
+  @struct case class Vicon(t: Time, p: Position, q: Attitude)
+  @struct case class State(v: Velocity, p: Position)
+  @struct case class Particle(w: Real, q: Quat, st: State, lastA: Acceleration)
+  @struct case class POSE(p: Position, q: Quat)
+
+  case class Mat(h: scala.Int, w: scala.Int, x: Array[Real]) { self =>
+    def *(y: Mat) = MatMult(self, y)
+    def +(y: Mat) = MatAdd(self, y)
+    def -(y: Mat) = MatMin(self, y)
+    def t         = transpose(self)
+  }
+
+  implicit class FIFOpeek[T](x: FIFO[T]) {
+    def peek: T = ???
+  }
+
+  implicit class Vec3Ops(x: Vec3) {
+    def *(y: Real) = Vec3(x.x*y, x.y*y, x.z*y)
+    def +(y: Real) = Vec3(x.x+y, x.y+y, x.z+y)
+    def +(y: Vec3) = Vec3(x.x+y.x, x.y+y.y, x.z+y.z)        
+  }
+
+  implicit class QuatOps(x: Quat) {
+    def *(y: Real) = Quat(x.r*y, x.i*y, x.j*y, x.k*y)
+    def *(y: Quat) = QuatMult(x, y)
+    def rotateBy(q: Quat) = q*x
+    def inverse = QuatInverse(x)
+  }
+
 
   @virtualize def prog() = {
 
-    val inIMU  = StreamIn[SReal](In2)        
-    val inV  = StreamIn[SQuaternion](In1)
-    val out = StreamOut[SQuaternion](Out1)
-    val p = 100 (1 -> 100)
+    val inIMU = StreamIn[IMU](In2)
+    val inV   = StreamIn[Vicon](In1)
+    val out   = StreamOut[POSE](Out1)
+    val parFactor = N (1 -> N)
 
     Accel {
-      val state = SRAM[SQuaternion](N)
-      val fifoV = FIFO[SQuaternion](100)
-      val fifoIMU = FIFO[SReal](100)
 
-      val lastV = Reg[SQuaternion]
-      val lastIMU = Reg[SReal]
+      val particles = SRAM[Particle](N)
+      val fifoIMU   = FIFO[IMU](100)
+      val fifoV     = FIFO[Vicon](100)
 
+      val lastTime = Reg[Time](initTime)
+      val lastO    = Reg[Omega](Vec3(0.0, 0.0, 0.0))
 
-      Foreach(N by 1)(x => {
-        val g1 = gaussian(0.0, 1.0)
-        val g2 = gaussian(0.0, 1.0)        
-        state(x) = SQuaternion(g1._1, g1._2, g2._1, g2._2)
-      })
-      
-      Stream(*)(x => {
-        fifoV.enq(inV)
-      })
+      Sequential {
 
-      Stream(*)(x => {
-        fifoIMU.enq(inIMU)
-      })
-      
-      FSM[Int]{x => x < 10}{x => {
-        out := state(x)
-        if (fifoV.empty && fifoIMU.empty)
-          ()
-        else if (fifoV.empty) {
-          println(fifoIMU.deq())
-        } else {
-          println(fifoV.deq())
-          ()
+        Foreach(N by 1 par parFactor)(x => {
+          particles(x) = Particle(1 / N,
+                                  Quat(initQ._1, initQ._2, initQ._3, initQ._4),
+                                  State(Vec3(initP._1, initP._2, initP._3), Vec3(initV._1, initV._2, initV._3)),
+                                  Vec3(0.0, 0.0, 0.0))
+        })
+
+        Parallel {
+          Stream(*)(x => {
+            fifoV.enq(inV)
+          })
+
+          Stream(*)(x => {
+            fifoIMU.enq(inIMU)
+          })
+
+          FSM[Boolean](x => x)(x => {
+            if ((fifoV.empty && !fifoIMU.empty) || (!fifoIMU.empty && !fifoV.empty && fifoIMU.peek.t < fifoV.peek.t))
+              updateFromIMU(fifoIMU.deq(), lastTime, lastO, particles, parFactor)
+            else if (!fifoV.empty)
+              updateFromV(fifoV.deq(), lastTime, lastO, particles, parFactor)
+
+            out := averagePOSE(particles)
+
+          })(x => fifoV.empty && fifoIMU.empty)
         }
-      }}{x => x + 1}
-
+      }
     }
+  }
+
+  @virtualize def updateAttitude(dt: Time, lastO: Reg[Omega], particles: SRAM1[Particle], parFactor: Int) = {
+    Foreach(N by 1 par parFactor)(i => {
+      val pp = particles(i)
+      val nq = sampleAtt(pp.q, lastO, dt)
+      particles(i) = Particle(pp.w, nq, pp.st, pp.lastA)
+    })
+  }
+
+  def updateFromIMU(x: IMU, lastTime: Reg[Time], lastO: Reg[Omega], particles: SRAM1[Particle], parFactor: Int) = {
+    val dt = x.t - lastTime
+    lastTime := x.t
+    lastO := x.g
+    updateAttitude(dt, lastO, particles, parFactor)
+  }
+
+  def sampleAtt(q: Quat, om: Omega, dt: Time): Quat = {
+    val withNoise  = gaussianVec(om, covGyro)
+    val integrated = withNoise * dt
+    val lq         = localAngleToQuat(integrated)
+    lq.rotateBy(q)
+  }
+  
+
+  def updateFromV(x: Vicon, lastTime: Reg[Time], lastO: Reg[Omega], particles: SRAM1[Particle], parFactor: Int) = {
+    val dt = x.t - lastTime
+    lastTime := x.t
+
+  }
+
+  def gaussianVec(mean: Vec3, variance: Real) = {
+    val g1 = gaussian()
+    val g2 = gaussian()
+    (Vec3(g1._1, g1._2, g2._1)*sqrt(variance)) + mean
   }
 
   //Box-Muller
   //http://www.design.caltech.edu/erik/Misc/Gaussian.html
-  @virtualize def gaussian(mean: SReal, variance: SReal) = {
+  @virtualize def gaussian() = {
 
-    val x1 = Reg[SReal]
-    val x2 = Reg[SReal]
-    val w = Reg[SReal]
-    do {
-      x1 := 2.0 * random[SReal](1.0) - 1.0
-      x2 := 2.0 * random[SReal](1.0) - 1.0
+    val x1 = Reg[Real]
+    val x2 = Reg[Real]
+    val w  = Reg[Real]
+    val w2 = Reg[Real]
+
+    FSM[Boolean](x => x)(x => {
+      x1 := 2.0 * random[Real](1.0) - 1.0
+      x2 := 2.0 * random[Real](1.0) - 1.0
       w := (x1 * x1 + x2 * x2)
-      false
-    } while (w.value >= 1.0)
+    })(x => w.value >= 1.0)
 
-    w := sqrt( (-2.0 * log(w.value) ) / w )
-    val y1 = x1 * w
-    val y2 = x2 * w
+    w2 := sqrt((-2.0 * log(w.value)) / w)
+
+    val y1 = x1 * w2;
+    val y2 = x2 * w2;
 
     (y1, y2)
-    
   }
 
-  def quatMult(q1: SQuaternion, q2: SQuaternion) = {
-    SQuaternion(
-      q1.r*q2.r - q1.i*q2.i - q1.j*q2.j - q1.k*q2.k,
-      q1.r*q2.i + q1.i*q2.r + q1.j*q2.k - q1.k*q2.j,
-      q1.r*q2.j - q1.i*q2.k + q1.j*q2.r + q1.k*q2.i,
-      q1.r*q2.k + q1.i*q2.j - q1.j*q2.i + q1.k*q2.r
+  def QuatMult(q1: Quat, q2: Quat) = {
+    Quat(
+      q1.r * q2.r - q1.i * q2.i - q1.j * q2.j - q1.k * q2.k,
+      q1.r * q2.i + q1.i * q2.r + q1.j * q2.k - q1.k * q2.j,
+      q1.r * q2.j - q1.i * q2.k + q1.j * q2.r + q1.k * q2.i,
+      q1.r * q2.k + q1.i * q2.j - q1.j * q2.i + q1.k * q2.r
     )
   }
 
-  def quatInverse(q: SQuaternion) = {
-    val n = q.r*q.r + q.i*q.i + q.j*q.j + q.k*q.k
-    SQuaternion(q.r/n, -q.i/n, -q.j/n, q.j/n)
+  def QuatInverse(q: Quat) = {
+    val n = q.r * q.r + q.i * q.i + q.j * q.j + q.k * q.k
+    Quat(q.r, -q.i, -q.j, q.j)*(1/n)
   }
 
-  def localAngleToQuat(v: SVec3): SQuaternion =
+  def norm(v: Vec3) =
+    v.x * v.x + v.y * v.y + v.z * v.z
+
+  def localAngleToQuat(v: Vec3): Quat = {
+    val n    = norm(v)
+    val l    = n / 2
+    val sl   = sin(l)
+    val nrot = v*(l/n)
+    Quat(cos(l), nrot.x, nrot.y, nrot.z)
+  }
+
+  def MatMult(a: Mat, b: Mat): Mat =
     ???
+
+  def MatAdd(a: Mat, b: Mat): Mat =
+    ???
+
+  def MatMin(a: Mat, b: Mat): Mat =
+    ???
+
+  def kalmanPredict(xp: Mat, sp: Mat, f: Mat, u: Mat, q: Mat) = {
+    val xm   = f * xp + u
+    val sigm = f * sp * f.t + q
+    (xm, sigm)
+  }
+
+  def inv(x: Mat): Mat =
+    ???
+
+  def transpose(x: Mat): Mat =
+    ???
+
+  def kalmanUpdate(xm: Mat, sigm: Mat, z: Mat, h: Mat, r: Mat) = {
+    val s   = h * sigm * h.t + r
+    val za  = h * xm
+    val k   = sigm * h.t * inv(s)
+    val sig = sigm - k * s * k.t
+    val x   = xm + k * (z - za)
+    (x, sig, (za, s))
+  }
+
+  def averagePOSE(x: SRAM[Particle]): POSE = {
+    ???
+  }
 
   val outs = List(Out1)
 
-
   val inputs = Map[Bus, List[MetaAny[_]]](
-    (In1 -> List[SReal](3f, 4f, 2f, 6f).map(x => SQuaternion(x, x, x, x))),
-    (In2 -> List[SReal](3f, 4f, 2f, 6f))
+    (In1 -> List[Real](3f, 4f, 2f, 6f).map(x => Quat(x, x, x, x))),
+    (In2 -> List[Real](3f, 4f, 2f, 6f))
   )
-  
-  
 
 }
 
-object ParticleFilterInterpreter extends ParticleFilter with SpatialStreamInterpreter 
+object ParticleFilterInterpreter extends ParticleFilter with SpatialStreamInterpreter
 
 object ParticleFilterCompiler extends ParticleFilter with SpatialApp {
   def main() =
     prog()
 }
-
-
-
