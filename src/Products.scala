209cbe6ec18f1458fbdc5bbb8abd5a396dc3aa8b
David Koeplinger <dkoeplinger@gmail.com>
2017-07-19 18:01:47 -0700

    update ranges for products

--- src/Products.scala
+++ src/Products.scala
@@ -5,16 +5,16 @@ object OuterProduct extends SpatialApp { // Regression (Dense) // Args: 640 640
 
   type X = FixPt[TRUE,_32,_0]
 
-  val ip = 8
+  val ip = 256
   val op = 1
 
-  val tileSize1 = 32
-  val tileSize2 = 32
+  val tileSize1 = 192
+  val tileSize2 = 96
 
   @virtualize
   def outerproduct[T:Type:Num](a: Array[T], b: Array[T]) = {
-    val tileSizeA = tileSize1 (64 -> 64 -> 38400)
-    val tileSizeB = tileSize2 (64 -> 64 -> 38400)
+    val tileSizeA = tileSize1 (64 -> 64 -> 5760)
+    val tileSizeB = tileSize2 (64 -> 64 -> 5760)
     val outerPar  = op (1 -> 4)
     val innerPar  = ip (1 -> 256)
 
@@ -84,17 +84,17 @@ object DotProduct extends SpatialApp { // Regression (Dense) // Args: 640
 
   type X = FixPt[TRUE,_32,_0]
 
-  val innerPar = 16
-  val outerPar = 2
+  val innerPar = 96
+  val outerPar = 1
 
-  val tileSize = 32
+  val tileSize = 1920
 
   @virtualize
   def dotproduct[T:Type:Num](aIn: Array[T], bIn: Array[T]): T = {
-    val B  = tileSize (64 -> 64 -> 19200) // 300
+    val B  = tileSize (64 -> 64 -> 9600) // 150
     val P1 = outerPar (1 -> 6) // 6
-    val P2 = innerPar (1 -> 192) // 192
-    val P3 = innerPar (1 -> 192) // 192
+    val P2 = innerPar (1 -> 256) // 256
+    val P3 = innerPar (1 -> 256) // 256
 
     val size = aIn.length; bound(size) = 1920000
 
