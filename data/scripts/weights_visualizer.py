c513cfa7fdf5b1bd0c87c6c70c4452a4842d7f54
kelayamatoz <tianzhao@stanford.edu>
2017-08-26 07:31:00 -0700

    restructure dir

---
+++ data/scripts/weights_visualizer.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numpy import genfromtxt
+
+
+'''
+f-kernel-d_cell4_bw-1500-100.csv
+f-kernel-d_cell4_fw-1500-100.csv
+f-kernel-first_cell_bw-300-100.csv
+f-kernel-first_cell_fw-300-100.csv
+f-kernel-second_cell_bw-900-100.csv
+f-kernel-second_cell_fw-900-100.csv
+i-bias-d_cell4_bw-1500-100.csv
+i-bias-d_cell4_fw-1500-100.csv
+i-bias-first_cell_bw-300-100.csv
+i-bias-first_cell_fw-300-100.csv
+i-bias-second_cell_bw-900-100.csv
+i-bias-second_cell_fw-900-100.csv
+i-kernel-d_cell4_bw-1500-100.csv
+i-kernel-d_cell4_fw-1500-100.csv
+i-kernel-first_cell_bw-300-100.csv
+i-kernel-first_cell_fw-300-100.csv
+i-kernel-second_cell_bw-900-100.csv
+i-kernel-second_cell_fw-900-100.csv
+j-bias-d_cell4_bw-1500-100.csv
+j-bias-d_cell4_fw-1500-100.csv
+j-bias-first_cell_bw-300-100.csv
+j-bias-first_cell_fw-300-100.csv
+j-bias-second_cell_bw-900-100.csv
+j-bias-second_cell_fw-900-100.csv
+j-kernel-d_cell4_bw-1500-100.csv
+j-kernel-d_cell4_fw-1500-100.csv
+j-kernel-first_cell_bw-300-100.csv
+j-kernel-first_cell_fw-300-100.csv
+j-kernel-second_cell_bw-900-100.csv
+j-kernel-second_cell_fw-900-100.csv
+o-kernel-d_cell4_bw-1500-100.csv
+o-kernel-d_cell4_fw-1500-100.csv
+o-kernel-first_cell_bw-300-100.csv
+o-kernel-first_cell_fw-300-100.csv
+o-kernel-second_cell_bw-900-100.csv
+o-kernel-second_cell_fw-900-100.csv
+'''
+
+csvs = []
