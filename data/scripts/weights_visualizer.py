d5841499d32792a3d01ad511ea8e5d1c4776fd61
kelayamatoz <tianzhao@stanford.edu>
2017-08-28 22:18:21 -0700

    visualize all kernels of the cells

--- data/scripts/weights_visualizer.py
+++ data/scripts/weights_visualizer.py
@@ -42,66 +42,51 @@ o-kernel-second_cell_bw-900-100.csv
 o-kernel-second_cell_fw-900-100.csv
 '''
 
-'''
-# make these smaller to increase the resolution
-dx, dy = 0.15, 0.05
+def draw_kernel(csvs, name):
+    matrices = [ genfromtxt('../bi-att-flow/'+file_name, delimiter=',') for file_name in csvs ]
+    cat_matrix = np.concatenate(matrices, axis=1)
+    plt.figure()
+    plt.imshow(cat_matrix)
+    plt.colorbar()
+    plt.savefig(name+'.pdf')
 
-# generate 2 2d grids for the x & y bounds
-y, x = np.mgrid[slice(-3, 3 + dy, dy),
-                slice(-3, 3 + dx, dx)]
-z = (1 - x / 2. + x ** 5 + y ** 3) * np.exp(-x ** 2 - y ** 2)
-# x and y are bounds, so z should be the value *inside* those bounds.
-# Therefore, remove the last value from the z array.
-z = z[:-1, :-1]
-z_min, z_max = -np.abs(z).max(), np.abs(z).max()
+csvs_first_cell_fw  =  ['f-kernel-first_cell_fw-300-100.csv', \
+                        'i-kernel-first_cell_fw-300-100.csv', \
+                        'j-kernel-first_cell_fw-300-100.csv', \
+                        'o-kernel-first_cell_fw-300-100.csv']
 
+csvs_first_cell_bw  =  ['f-kernel-first_cell_bw-300-100.csv', \
+                        'i-kernel-first_cell_bw-300-100.csv', \
+                        'j-kernel-first_cell_bw-300-100.csv', \
+                        'o-kernel-first_cell_bw-300-100.csv']
 
-plt.subplot(2, 2, 1)
-plt.pcolor(x, y, z, cmap='RdBu', vmin=z_min, vmax=z_max)
-plt.title('pcolor')
-# set the limits of the plot to the limits of the data
-plt.axis([x.min(), x.max(), y.min(), y.max()])
-plt.colorbar()
+csvs_second_cell_fw  = ['f-kernel-second_cell_fw-900-100.csv', \
+                        'i-kernel-second_cell_fw-900-100.csv', \
+                        'j-kernel-second_cell_fw-900-100.csv', \
+                        'o-kernel-second_cell_fw-900-100.csv']
 
+csvs_second_cell_bw  = ['f-kernel-second_cell_bw-900-100.csv', \
+                        'i-kernel-second_cell_bw-900-100.csv', \
+                        'j-kernel-second_cell_bw-900-100.csv', \
+                        'o-kernel-second_cell_bw-900-100.csv']
 
-plt.subplot(2, 2, 2)
-plt.pcolormesh(x, y, z, cmap='RdBu', vmin=z_min, vmax=z_max)
-plt.title('pcolormesh')
-# set the limits of the plot to the limits of the data
-plt.axis([x.min(), x.max(), y.min(), y.max()])
-plt.colorbar()
+csvs_d_cell4_fw  = ['f-kernel-d_cell4_fw-1500-100.csv', \
+                        'i-kernel-d_cell4_fw-1500-100.csv', \
+                        'j-kernel-d_cell4_fw-1500-100.csv', \
+                        'o-kernel-d_cell4_fw-1500-100.csv']
 
+csvs_d_cell4_bw = ['f-kernel-d_cell4_bw-1500-100.csv', \
+                        'i-kernel-d_cell4_bw-1500-100.csv', \
+                        'j-kernel-d_cell4_bw-1500-100.csv', \
+                        'o-kernel-d_cell4_bw-1500-100.csv']
 
-plt.subplot(2, 2, 3)
-plt.imshow(z, cmap='RdBu', vmin=z_min, vmax=z_max,
-           extent=[x.min(), x.max(), y.min(), y.max()],
-           interpolation='nearest', origin='lower')
-plt.title('image (nearest)')
-plt.colorbar()
+pairs = [(csvs_first_cell_bw, 'csvs_first_cell_bw'), \
+         (csvs_first_cell_fw, 'csvs_first_cell_fw'), \
+         (csvs_second_cell_fw, 'csvs_second_cell_fw'), \
+         (csvs_second_cell_bw, 'csvs_second_cell_bw'), \
+         (csvs_d_cell4_fw, 'csvs_d_cell4_fw'), \
+         (csvs_d_cell4_bw, 'csvs_d_cell4_bw')]
 
-
-ax = plt.subplot(2, 2, 4)
-ax.pcolorfast(x, y, z, cmap='RdBu', vmin=z_min, vmax=z_max)
-plt.title('pcolorfast')
-plt.colorbar()
-
-plt.subplots_adjust(wspace=0.5, hspace=0.5)
-
-plt.show()
-Keywords: python, matplotlib, pylab, example, codex (see Search examples)
-
-© Copyright 2002 - 2012 John Hunter, Darren Dale, Eric Firing, Michael Droettboom and the Matplotlib development team; 2012 - 2016 The Matplotlib development team. Last updated on May 10, 2017. Created using Sphinx 1.5.5.
-'''
-
-csvs = ['f-kernel-first_cell_fw-300-100.csv', \
-        'i-kernel-first_cell_fw-300-100.csv', \
-        'j-kernel-first_cell_fw-300-100.csv', \
-        'o-kernel-first_cell_fw-300-100.csv']
-
-matrices = [ genfromtxt('../bi-att-flow/'+file_name, delimiter=',') for file_name in csvs ]
-cat_matrix = np.concatenate(matrices, axis=1)
-plt.figure()
-plt.imshow(cat_matrix)
-plt.colorbar()
-
-plt.savefig('first_cell_kernel.pdf')
+for pair in pairs:
+    cell, cell_name = pair
+    draw_kernel(cell, cell_name)
