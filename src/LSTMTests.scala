5aea6585a215f49907a14055a5353e4b824ae363
kelayamatoz <zhaotian1991713@gmail.com>
2017-09-18 17:30:34 -0700

    adding matmult

--- src/LSTMTests.scala
+++ src/LSTMTests.scala
@@ -10,6 +10,8 @@ trait Activations extends SpatialApp {
   val revPrec = 2
   val totalSize = 128
 
+
+  // TODO: seems that the aggregated output is quite off. need to fix it...
   // 128 elements, inputs: -32 to 32, outputs: 0 to 1
   def sigmoid[T:Type:Num](p: T) = {
     val sigmoidDir = projectDir + "apps/src/__128_sigmoidLUT.csv"
@@ -18,6 +20,8 @@ trait Activations extends SpatialApp {
     val upperMux = mux(p > lo.to[T], 1.to[T], lowerMux)
     upperMux
   }
+
+
   // 128 elements, inputs: -32 to 32, outputs: -1 to 1
   def tanh[T:Type:Num](p: T) = {
     val tanhDir = projectDir + "apps/src/__128_tanhLUT.csv"
@@ -48,7 +52,7 @@ trait LUTBase extends SpatialApp with Activations {
 
 trait LSTMParams_dcell4 extends SpatialApp {
   val kernelSize = (1500, 100)
-  val hiddenSize = 100
+  val d = 100
   val N = 60
   val M = 1
   val JX = 161
@@ -57,16 +61,6 @@ trait LSTMParams_dcell4 extends SpatialApp {
   val internalPath = "/home/tianzhao/spatial-lang/apps/LSTM-internals/"
   val inputPath = internalPath + "IOs/input-0.csv"
   val outputPath = internalPath + "IOs/output-0.csv"
-  val kernelPath_i = internalPath + "weights/i-kernel-d_cell4_fw-1500-100.csv" 
-  val kernelPath_j = internalPath + "weights/j-kernel-d_cell4_fw-1500-100.csv" 
-  val kernelPath_f = internalPath + "weights/f-kernel-d_cell4_fw-1500-100.csv" 
-  val kernelPath_o = internalPath + "weights/o-kernel-d_cell4_fw-1500-100.csv" 
-  val biasPath_i = internalPath + "weights/i-bias-d_cell4_fw-1500-100.csv"
-  val biasPath_j = internalPath + "weights/j-bias-d_cell4_fw-1500-100.csv"
-  val biasPath_f = internalPath + "weights/f-bias-d_cell4_fw-1500-100.csv"
-  val biasPath_o = internalPath + "weights/o-bias-d_cell4_fw-1500-100.csv"
-
-  // Paths for the full kernel 
   val kernelPath = internalPath + "weights/kernel-d_cell4_fw-1500-400.csv"
   val biasPath = internalPath + "weights/bias-d_cell4_fw-400.csv"
 }
@@ -74,31 +68,57 @@ trait LSTMParams_dcell4 extends SpatialApp {
 
 trait Prepro extends SpatialApp with LSTMParams_dcell4 { 
   def prepro[T:Type:Num]()(implicit cast: Cast[MString, T]) = {
-    val ik = loadCSV2D[T](kernelPath_i, ",", "\n")
-    val jk = loadCSV2D[T](kernelPath_j, ",", "\n")
-    val fk = loadCSV2D[T](kernelPath_f, ",", "\n")
-    val ok = loadCSV2D[T](kernelPath_o, ",", "\n")
-    val ib = loadCSV2D[T](biasPath_i, ",", "\n")
-    val jb = loadCSV2D[T](biasPath_j, ",", "\n")
-    val fb = loadCSV2D[T](biasPath_f, ",", "\n")
-    val ob = loadCSV2D[T](biasPath_o, ",", "\n")
-    // val k = loadCSV2D[T](kernelPath, ",", "\n")
-    // val b = loadCSV2D[T](biasPath_i, ",", "\n")
+    val k = loadCSV2D[T](kernelPath, ",", "\n")
+    val b = loadCSV2D[T](biasPath, ",", "\n")
     val inHidden = loadCSV2D[T](inputPath, ",", "\n")
-    // (ik, jk, fk, ok, ib, jb, fb, ob, inHidden)
-    
+    (k, b, inHidden)
   }
+}
+
+
+// This traits contain different flavors of matmult
+trait MatMult extends SpatialApp with LSTMParams_dcell4 {
+  // MatMult parallelization factors
+  // val innerPar = 16
+  // val midPar = 2
+  // val outerPar = 2
+  val op = 2
+  val mp = 2 
+  val ip = 16
+  val px = 1 (1 -> 1) 
+  val MM = N*M*JX
+  val NN = 4*d
+
+  def MatMult[T:Type:Num](a: DRAM2[T], b: DRAM2[T], c: DRAM2[T])
+      Foreach(MM by bm, NN by bn par op){(i,j) =>
+        val tileC = SRAM[T](bm, bn)
+
+        Foreach(P by bp par px){k =>
+          val tileA = SRAM[T](bm, bp)
+          val tileB = SRAM[T](bp, bn)
+          Parallel {
+            tileA load a(i::i+bm, k::k+bp par 1) // Reads M*N*P times
+            tileB load b(k::k+bp, j::j+bn par 1)
+          }
+          Foreach(bm by 1, bn by 1 par mp){ (ii,jj) =>    // MetaPipe?
+            val prod = Reduce(Reg[T])(bp by 1 par ip){kk => tileA(ii, kk) * tileB(kk, jj) }{_+_}
+            val prev = mux(k == 0, 0.to[T], tileC(ii,jj))
+            tileC(ii,jj) = prev + prod.value // Is a unit pipe that should be recognized as accum
+          }
+        }
+        c(i::i+bm, j::j+bn) store tileC // Writes M*N times
+      }
+    }
 }
 
 
 // Cell implementation
 trait BasicLSTMCell_16_16 extends SpatialApp with Prepro
-                                             with Activations {
-  def CellForward[T:Type:Num](dramTuple: (DRAM2[T], DRAM2[T], DRAM2[T], 
-                                            DRAM2[T], DRAM2[T], DRAM2[T], 
-                                            DRAM2[T], DRAM2[T], DRAM2[T]))(implicit cast: Cast[MString, T]) = {
-    val (ikdram, jkdram, fkdram, okdram, ibdram, jbdram, fbdram, obdram, inhdram) = dramTuple
-
+                                             with Activations
+                                             with MatMult {
+  def CellForward[T:Type:Num](dramTuple: (DRAM2[T], DRAM[T], DRAM2[T]))(implicit cast: Cast[MString, T]) = {
+    val (kdram, bdram, inhiddram) = dramTuple
+   
   }
 }
 
@@ -108,26 +128,14 @@ trait BasicLSTMCell_16_16 extends SpatialApp with Prepro
 trait RNN extends SpatialApp with BasicLSTMCell_16_16 
                               with Prepro {
   def RNNForward[T:Type:Num]()(implicit cast: Cast[MString, T]) {
-    val (ik, jk, fk, ok, ib, jb, fb, ob, inHidden) = prepro[T]()
-    val ikdram = DRAM[T](idco, hiddenSize)
-    val jkdram = DRAM[T](idco, hiddenSize)
-    val fkdram = DRAM[T](idco, hiddenSize) 
-    val okdram = DRAM[T](idco, hiddenSize)
-    val ibdram = DRAM[T](idco, hiddenSize)
-    val jbdram = DRAM[T](idco, hiddenSize)
-    val fbdram = DRAM[T](idco, hiddenSize)
-    val obdram = DRAM[T](idco, hiddenSize)
-    val inhdram = DRAM[T](N*M*JX, (idco+hiddenSize))
-    setMem(ikdram, ik)
-    setMem(jkdram, jk)
-    setMem(fkdram, fk)
-    setMem(okdram, ok)
-    setMem(ibdram, ib)
-    setMem(jbdram, jb)
-    setMem(fbdram, fb)
-    setMem(obdram, ob)
-    setMem(inhdram, inHidden)
-    val dramTuple = (ikdram, jkdram, fkdram, okdram, ibdram, jbdram, fbdram, obdram, inhdram)
+    val (k, b, inHidden) = prepro[T]()
+    val kdram = DRAM[T](idco+d, d*4)
+    val bdram = DRAM[T](d*4)
+    val inhiddram = DRAM[T](N*M*JX, (idco+d))
+    setMem(kdram, k)
+    setMem(bdram, b)
+    setMem(inhiddram, inHidden)
+    val dramTuple = (kdram, bdram, inhiddram)
     // TODO: how to compress input x? It needs to be of a dynamic shape
     // TODO: for now let's assume that it's of fixed shape 161 as JX 
     Accel {
