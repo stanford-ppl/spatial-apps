0999e23ea7755cab6bb0e0ec730774fb969d58fb
kelayamatoz <zhaotian1991713@gmail.com>
2018-01-17 21:45:58 -0800

    added apps for memory tests

---
+++ src/MemoryTests.scala
@@ -0,0 +1,57 @@
+import spatial.dsl._
+import org.virtualized._
+import spatial.stdlib._
+import spatial.targets._
+
+
+object SRAMRWTest extends SpatialApp {
+  @virtualize
+  def main() {
+    type T = Int
+    val memSize = 16
+
+    val x = ArgIn[Int]
+    val y = ArgOut[Int]
+    val N = args(0).to[Int]
+
+    setArg(x, N)
+
+    Accel {
+      Pipe {
+        val fpgaMem = SRAM[T](memSize)
+        Foreach(memSize by 1) { i =>
+          fpgaMem(i) = i + 1
+        }
+
+        y := fpgaMem(3) + x
+      }
+    }
+
+    val result = getArg(y)
+    val gold = N + 4
+    println("result = " + result)
+    println("gold = " + gold)
+  }
+}
+
+
+object DRAMWriteTest extends SpatialApp {
+  @virtualize
+  def main() {
+    type T = Int
+    val memSize = 16
+    val re = DRAM[T](memSize)
+
+    Accel {
+      val fpgaMem = SRAM[T](memSize)
+      Foreach(memSize by 1) { i =>
+        fpgaMem(i) = i
+      }
+
+      re(0::0+memSize) store fpgaMem
+    }
+
+    val testRe = getMem(re)
+    printArray(testRe, " Result is")
+  }
+}
