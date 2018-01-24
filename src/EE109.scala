3a05f204df0d557686e04d049e30ba75b1f52a3c
kelayamatoz <zhaotian1991713@gmail.com>
2018-01-24 15:50:46 -0800

    added a few apps for memory tests and for ee109 demo cases

---
+++ src/EE109.scala
@@ -0,0 +1,15 @@
+import spatial.dsl._
+import org.virtualized._
+import spatial.stdlib._
+import spatial.targets._
+
+
+object BasicDotProduct extends SpatialApp {
+  @virtualize
+
+  def main() {
+    type T = Int
+
+
+  }
+}
