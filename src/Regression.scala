3449e45e51913780eefff7f84d697c06795fd84b
Matthew Feldman <mattfel@stanford.edu>
2018-02-24 21:06:23 -0800

    update special nodse and add svrg

--- src/Regression.scala
+++ src/Regression.scala
@@ -39,6 +39,7 @@ object Regression {
     dense ::= (TPCHQ6, Array(9600000))
     dense ::= (SGD_minibatch, Array(40, 64, 0.0001))
     dense ::= (SGD, Array(40, 64, 0.0001))
+    dense ::= (SVRG, Array(25, 30, 256, 0.0001, 0.0009, 10))
     dense ::= (Gibbs_Ising2D, Array(25, 0.3, 2))
     dense ::= (GDA, Array(1024))
     dense ::= (Differentiator, NoArgs)
