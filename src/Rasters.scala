81b487865760424849694e288c81b156278f6c46
kelayamatoz <zhaotian1991713@gmail.com>
2018-03-06 16:14:42 -0800

    updated the rasters apps with arria10 supports. Need to validate the writes to frame buffer

--- src/Rasters.scala
+++ src/Rasters.scala
@@ -2,6 +2,49 @@ import spatial.dsl._
 import org.virtualized._
 import spatial.targets.DE1
 
+
+
+object MovingBoxA10 extends SpatialApp {
+  override val target = Arria10
+
+  val Cmax = 1080
+  val Rmax = 720
+
+  // TODO: on Arria10, Pixel16 is really Pixel32...
+  @struct case class Pixel16(b: UInt5, g: UInt6, r: UInt5)
+
+  @virtualize
+  def convolveVideoStream(): Unit = {
+    val imgOut = BufferedOut[Pixel16](DE1.VGA)
+    val dwell = ArgIn[Int]
+    val d = args(0).to[Int]
+    setArg(dwell, d)
+
+    Accel (*) {
+      Foreach(0 until 4) { i =>
+        Foreach(0 until dwell) { _ =>
+          Foreach(0 until Rmax, 0 until Cmax){ (r, c) =>
+            val pixel1 = mux(r > 60 && r < 120 && c > 60 && c < 120, Pixel16(0,63,0), Pixel16(0,0,0))
+            val pixel2 = mux(r > 120 && r < 180 && c > 60 && c < 120, Pixel16(31,0,0), Pixel16(0,0,0))
+            val pixel3 = mux(r > 120 && r < 180 && c > 120 && c < 180, Pixel16(0,0,31), Pixel16(0,0,0))
+            val pixel4 = mux(r > 60 && r < 120 && c > 120 && c < 180, Pixel16(31,0,0), Pixel16(0,0,0))
+            val pixel = mux(i == 0, pixel1, mux( i == 1, pixel2, mux( i == 2, pixel3, mux(i == 3, pixel4, Pixel16(0,0,0)))))
+            imgOut(r, c) = pixel
+          }
+        }
+      }
+    }
+  }
+
+  @virtualize
+  def main() {
+    val R = Rmax
+    val C = Cmax
+    convolveVideoStream()
+  }
+}
+
+
 object MovingBox extends SpatialApp {
   override val target = DE1
 
@@ -18,7 +61,7 @@ object MovingBox extends SpatialApp {
     setArg(dwell, d)
 
     Accel (*) {
-      Foreach(0 until 4) { i => 
+      Foreach(0 until 4) { i =>
         Foreach(0 until dwell) { _ =>
           Foreach(0 until Rmax, 0 until Cmax){ (r, c) =>
             val pixel1 = mux(r > 60 && r < 120 && c > 60 && c < 120, Pixel16(0,63,0), Pixel16(0,0,0))
@@ -58,8 +101,8 @@ object ColoredLines extends SpatialApp { // try arg = 100
     val imgOut = BufferedOut[Pixel16](target.VGA)
 
     Accel (*) {
-      Foreach(0 until Rmax) { i => 
-        Foreach(0 until dwell) { _ => 
+      Foreach(0 until Rmax) { i =>
+        Foreach(0 until dwell) { _ =>
           Foreach(0 until Rmax, 0 until Cmax){ (r, c) =>
             val bluepixel = mux(r == i, Pixel16((r%32).as[UInt5],0,0), Pixel16(0,0,0))
             val greenpixel = mux(r == i, Pixel16(0,(r%32).as[UInt6],0), Pixel16(0,0,0))
@@ -101,20 +144,20 @@ object LinebufRaster extends SpatialApp { // try arg = 100
 
     Accel (*) {
       val lb = LineBuffer[Pixel16](1,Cmax)
-      Foreach(0 until Rmax) { i => 
-        Foreach(0 until dwell) { _ => 
+      Foreach(0 until Rmax) { i =>
+        Foreach(0 until dwell) { _ =>
           Foreach(0 until Rmax){ r =>
-            Foreach(0 until Cmax) { c => 
+            Foreach(0 until Cmax) { c =>
                 val bluepixel = mux(abs(r - i) < 4, Pixel16(31.to[UInt5],0,0), Pixel16(0,0,0))
                 val greenpixel = mux(abs(r - i) < 4, Pixel16(0,31.to[UInt6],0), Pixel16(0,0,0))
                 val redpixel = mux(abs(r - i) < 4, Pixel16(0,0,31.to[UInt5]), Pixel16(0,0,0))
                 val pixel = mux(c < Cmax/3, bluepixel, mux(c < Cmax*2/3, greenpixel, redpixel))
                 lb.enq(pixel)
             }
-            Foreach(0 until Cmax) { c => 
+            Foreach(0 until Cmax) { c =>
               imgOut(r, c) = lb(0, c)
             }
-            
+
           }
         }
       }
@@ -152,8 +195,8 @@ object ColorSelect extends SpatialApp { // try arg = 100
     val imgOut = BufferedOut[Pixel16](target.VGA)
 
     Accel (*) {
-      Foreach(0 until Rmax) { i => 
-        Foreach(0 until dwell) { _ => 
+      Foreach(0 until Rmax) { i =>
+        Foreach(0 until dwell) { _ =>
           Foreach(0 until Rmax, 0 until Cmax){ (r, c) =>
             val swBits = swInput.value()
             val bluepixel = mux(r == i, Pixel16(31.to[UInt5],0,0), Pixel16(0,0,0))
