d969da893c13be770075a44ee0d4c476ecf6a72d
kelayamatoz <zhaotian1991713@gmail.com>
2018-03-12 13:47:31 -0700

    commit lab1

--- src/lab1.scala
+++ src/lab1.scala
@@ -15,54 +15,285 @@ object Lab1Part1RegExample extends SpatialApp {
     val argRegOut = ArgOut[T]
 
     Accel {
-      val regDefault = Reg[T](2.to[T])
-      regDefault := 4.to[T]
-      val regDefaultValue = regDefault.value
       val argRegIn0Value = argRegIn0.value
       val argRegIn1Value = argRegIn1.value
-      argRegOut := regDefaultValue + argRegIn0Value + argRegIn1Value
+      argRegOut := argRegIn0Value + argRegIn1Value
     }
 
     val argRegOutResult = getArg(argRegOut)
     println("Result = " + argRegOutResult)
 
-    val gold = M + N + 4.to[T]
+    val gold = M + N
     println("Gold = " + gold)
-    val chksum = gold == argRegOutResult
-    println("PASS = " + chksum)
+    val cksum = gold == argRegOutResult
+    println("PASS = " + cksum)
   }
 }
 
 
-// object Lab1Part2DramSramExample extends SpatialApp {
-//
-// }
-//
-//
-// object Lab1Part3RegFileExample extends SpatialApp {
-//
-// }
-//
-// object Lab1Part4FIFOExample extends SpatialApp {
-//
-// }
-//
-// object Lab1Part5FILOExample extends SpatialApp {
-//
-// }
-//
-// object Lab1Part6CounterExample extends SpatialApp {
-//
-// }
-//
-// object Lab1Part7CounterChainExample extends SpatialApp {
-//
-// }
-//
-// object Lab1Part8LineBufferExample extends SpatialApp {
-//
-// }
-//
-// object Lab1Part9LUTExample extends SpatialApp {
-//
+object Lab1Part2DramSramExample extends SpatialApp {
+
+  val N = 32
+
+  @virtualize
+  def simpleLoadStore[T:Type:Num](srcHost: Array[T], value: T) = {
+    val tileSize = 16
+    val srcFPGA = DRAM[T](N)
+    val dstFPGA = DRAM[T](N)
+    setMem(srcFPGA, srcHost)
+
+    val x = ArgIn[T]
+    setArg(x, value)
+    Accel {
+      Sequential.Foreach(N by tileSize) { i =>
+        val b1 = SRAM[T](tileSize)
+
+        b1 load srcFPGA(i::i+tileSize)
+
+        val b2 = SRAM[T](tileSize)
+        Foreach(tileSize by 1) { ii =>
+          b2(ii) = b1(ii) * x
+        }
+
+        dstFPGA(i::i+tileSize) store b2
+      }
+    }
+    getMem(dstFPGA)
+  }
+
+  @virtualize
+  def main() {
+    val arraySize = N
+    val value = args(0).to[Int]
+
+    val src = Array.tabulate[Int](arraySize) { i => i % 256 }
+    val dst = simpleLoadStore(src, value)
+
+    val gold = src.map { _ * value }
+
+    println("Sent in: ")
+    (0 until arraySize) foreach { i => print(gold(i) + " ") }
+    println("Got out: ")
+    (0 until arraySize) foreach { i => print(dst(i) + " ") }
+    println("")
+
+    val cksum = dst.zip(gold){_ == _}.reduce{_&&_}
+    println("PASS: " + cksum)
+  }
+}
+
+object Lab1Part4FIFOExample extends SpatialApp {
+  val N = 32
+
+  @virtualize
+  def simpleLoadStore[T:Type:Num](srcHost: Array[T], value: T) = {
+    val tileSize = 16
+    val srcFPGA = DRAM[T](N)
+    val dstFPGA = DRAM[T](N)
+    setMem(srcFPGA, srcHost)
+
+    val x = ArgIn[T]
+    setArg(x, value)
+    Accel {
+      Sequential.Foreach(N by tileSize) { i =>
+        val b1 = FIFO[T](tileSize)
+
+        b1 load srcFPGA(i::i+tileSize)
+
+        val b2 = SRAM[T](tileSize)
+        Foreach(tileSize by 1) { ii =>
+          b2(ii) = b1.deq() * x
+        }
+
+        dstFPGA(i::i+tileSize) store b2
+      }
+    }
+    getMem(dstFPGA)
+  }
+
+  @virtualize
+  def main() {
+    val arraySize = N
+    val value = args(0).to[Int]
+
+    val src = Array.tabulate[Int](arraySize) { i => i % 256 }
+    val dst = simpleLoadStore(src, value)
+
+    val gold = src.map { _ * value }
+
+    println("Sent in: ")
+    (0 until arraySize) foreach { i => print(gold(i) + " ") }
+    println("Got out: ")
+    (0 until arraySize) foreach { i => print(dst(i) + " ") }
+    println("")
+
+    val cksum = dst.zip(gold){_ == _}.reduce{_&&_}
+    println("PASS: " + cksum)
+  }
+}
+
+
+object Lab1Part5FILOExample extends SpatialApp {
+  val N = 32
+
+  @virtualize
+  def simpleLoadStore[T:Type:Num](srcHost: Array[T], value: T) = {
+    val tileSize = 16
+    val srcFPGA = DRAM[T](N)
+    val dstFPGA = DRAM[T](N)
+    setMem(srcFPGA, srcHost)
+
+    val x = ArgIn[T]
+    setArg(x, value)
+    Accel {
+      Sequential.Foreach(N by tileSize) { i =>
+        val b1 = FILO[T](tileSize)
+
+        b1 load srcFPGA(i::i+tileSize)
+
+        val b2 = SRAM[T](tileSize)
+        Foreach(tileSize by 1) { ii =>
+          b2(tileSize - ii - 1) = b1.pop() * x
+        }
+
+        dstFPGA(i::i+tileSize) store b2
+      }
+    }
+    getMem(dstFPGA)
+  }
+
+  @virtualize
+  def main() {
+    val arraySize = N
+    val value = args(0).to[Int]
+
+    val src = Array.tabulate[Int](arraySize) { i => i % 256 }
+    val dst = simpleLoadStore(src, value)
+
+    val gold = src.map { _ * value }
+
+    println("Sent in: ")
+    (0 until arraySize) foreach { i => print(gold(i) + " ") }
+    println("Got out: ")
+    (0 until arraySize) foreach { i => print(dst(i) + " ") }
+    println("")
+
+    val cksum = dst.zip(gold){_ == _}.reduce{_&&_}
+    println("PASS: " + cksum)
+  }
+}
+
+
+object Lab1Part6ReduceExample extends SpatialApp {
+  val N = 32
+  val tileSize = 16
+  type T = Int
+
+  @virtualize
+  def main() {
+    val arraySize = N
+    val srcFPGA = DRAM[T](N)
+    val src = Array.tabulate[Int](arraySize) { i => i % 256 }
+    setMem(srcFPGA, src)
+    val destArg = ArgOut[T]
+
+    Accel {
+      val accum = Reg[T](0)
+      Sequential.Reduce(accum)(N by tileSize) { i =>
+        val b1 = SRAM[T](tileSize)
+        b1 load srcFPGA(i::i+tileSize)
+        Reduce(0)(tileSize by 1) { ii => b1(ii) }{_+_}
+      }{_+_}
+
+
+      destArg := accum.value
+    }
+
+    val result = getArg(destArg)
+    val gold = src.reduce{_+_}
+    println("Gold: " + gold)
+    println("Result: : " + result)
+    println("")
+
+    val cksum = gold == result
+    println("PASS: " + cksum)
+  }
+}
+
+
+object Lab1Part7FoldExample extends SpatialApp {
+  val N = 32
+  val tileSize = 16
+  type T = Int
+
+  @virtualize
+  def main() {
+    val arraySize = N
+    val srcFPGA = DRAM[T](N)
+    val src = Array.tabulate[Int](arraySize) { i => i % 256 }
+    setMem(srcFPGA, src)
+    val destArg = ArgOut[T]
+
+    Accel {
+      val accum = Reg[T](0)
+      Sequential.Fold(accum)(N by tileSize) { i =>
+        val b1 = SRAM[T](tileSize)
+        b1 load srcFPGA(i::i+tileSize)
+        Reduce(0)(tileSize by 1) { ii => b1(ii) }{_+_}
+      }{_+_}
+
+
+      destArg := accum.value
+    }
+
+    val result = getArg(destArg)
+    val gold = src.reduce{_+_}
+    println("Gold: " + gold)
+    println("Result: : " + result)
+    println("")
+
+    val cksum = gold == result
+    println("PASS: " + cksum)
+  }
+}
+
+
+object Lab1Part8MemReduceExample extends Spatialapp {
+  val N = 32
+  val tileSize = 16
+  type T = Int
+
+  @virtualize
+  def main() {
+    val arraySize = N
+    val srcFPGA = DRAM[T](N)
+    val src = Array.tabulate[Int](arraySize) { i => i % 256 }
+    setMem(srcFPGA, src)
+    val destArg = ArgOut[T]
+
+    Accel {
+      val accumMem = SRAM[T](N)
+      Sequential.Fold(accum)(N by tileSize) { i =>
+        val b1 = SRAM[T](tileSize)
+        b1 load srcFPGA(i::i+tileSize)
+        Reduce(0)(tileSize by 1) { ii => b1(ii) }{_+_}
+      }{_+_}
+
+
+      destArg := accum.value
+    }
+
+    val result = getArg(destArg)
+    val gold = src.reduce{_+_}
+    println("Gold: " + gold)
+    println("Result: : " + result)
+    println("")
+
+    val cksum = gold == result
+    println("PASS: " + cksum)
+  }
+}
+
+// object Lab1Part9MemFoldExample extends SpatialApp {
+
 // }
