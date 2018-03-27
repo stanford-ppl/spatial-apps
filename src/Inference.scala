9b3a1b1ded8208d59bc5f066c933b049f0e919a4
Matthew Feldman <mattfel@stanford.edu>
2018-03-27 15:08:12 -0700

    add relu and bit shifting to RCIO

--- src/Inference.scala
+++ src/Inference.scala
@@ -6,18 +6,22 @@ import spatial.targets._
 
 object SingleLayerConv_RCIO extends SpatialApp {
   type T = FixPt[TRUE,_16,_0]
+  type T2 = FixPt[TRUE,_32,_0]
+  type REALT = FixPt[TRUE,_4,_12]
+  type REALT2 = FixPt[TRUE,_8,_24]
 
   @virtualize
   def main() {
 
     val debug:scala.Boolean = false
 
-    val PX = 1
-    val P1 = 2 // Unsafe parallelization if OC < 1 burst (16)
-    val P2 = 2 // Unsafe parallelization if OC < 1 burst (16)
-    val P3 = 2
-    val P4 = 2
-    val P5 = 4
+    val PX = 1 //1
+    val P1 = 1 //2 // Unsafe parallelization if OC < 1 burst (16)
+    val P2 = 1 //2 // Unsafe parallelization if OC < 1 burst (16)
+    val P3 = 1 //2
+    val P4 = 1 //2
+    val P5 = 1 //4
+    val P6 = 1 //16
     val loadPar = 4 (1 -> 16)
     val storePar = 4 (1 -> 16)
     // Scalar params
@@ -53,17 +57,17 @@ object SingleLayerConv_RCIO extends SpatialApp {
     val INPUT_DATA = DRAM[T](INPUT_ROWS, INPUT_COLS, INPUT_CHANS)
     val OUTPUT_DATA = DRAM[T]((INPUT_ROWS-(KERNEL_ROWS-STRIDE))/STRIDE, (INPUT_COLS-(KERNEL_COLS-STRIDE))/STRIDE, OUTPUT_CHANS)
     val KERNEL_DATA = DRAM[T](KERNEL_ROWS, KERNEL_COLS, INPUT_CHANS, OUTPUT_CHANS)
-    val BIAS_DATA =   DRAM[T]((INPUT_ROWS-(KERNEL_ROWS-STRIDE))/STRIDE, (INPUT_COLS-(KERNEL_COLS-STRIDE))/STRIDE, OUTPUT_CHANS)
+    val BIAS_DATA =   DRAM[T](OUTPUT_CHANS)
 
     // Load data (placeholder)
-    val input = (0::INPUT_ROWS, 0::INPUT_COLS, 0::INPUT_CHANS) {(i,j,k) => ((i + j + k) % 8).to[T]}
+    val input = (0::INPUT_ROWS, 0::INPUT_COLS, 0::INPUT_CHANS) {(i,j,k) => ((i + j + k) % 64 - 8).to[T]}
     val output = (0::(INPUT_ROWS-(KERNEL_ROWS-STRIDE))/STRIDE, 0::(INPUT_COLS-(KERNEL_COLS-STRIDE))/STRIDE, 0::OUTPUT_CHANS){(i,j,k) => 0.to[T]}
-    val kernel = (0::KERNEL_ROWS, 0::KERNEL_COLS, 0::INPUT_CHANS, 0::OUTPUT_CHANS) {(i,j,k,l) => if (random[Int](10) > 8) 1.to[T] else 0.to[T]}
-    val bias =   (0::(INPUT_ROWS-(KERNEL_ROWS-STRIDE))/STRIDE, 0::(INPUT_COLS-(KERNEL_COLS-STRIDE))/STRIDE, 0::OUTPUT_CHANS){(i,j,k) => 1.to[T]}
+    val kernel = (0::KERNEL_ROWS, 0::KERNEL_COLS, 0::INPUT_CHANS, 0::OUTPUT_CHANS) {(i,j,k,l) => if (random[Int](10) > 7) 5.to[T] else 0.to[T]}
+    val bias =   Array.tabulate(OUTPUT_CHANS){i => 1.to[T]}
 
     printTensor3(input, "Input")
     printTensor4(kernel, "Kernel")
-    printTensor3(bias, "Bias")
+    printArray(bias, "Bias")
 
     // Set data
     setMem(INPUT_DATA, input)
@@ -76,7 +80,11 @@ object SingleLayerConv_RCIO extends SpatialApp {
 
     Accel{
       val kernel_sram = SRAM[T](KERNEL_ROWS, KERNEL_COLS, INPUT_CHANS_MAX, OUTPUT_CHANS_MAX)
-      kernel_sram load KERNEL_DATA(0::KERNEL_ROWS, 0::KERNEL_COLS, 0::INPUT_CHANS, 0::OUTPUT_CHANS)
+      val bias_sram = SRAM[T](OUTPUT_CHANS_MAX)
+      Parallel{
+        kernel_sram load KERNEL_DATA(0::KERNEL_ROWS, 0::KERNEL_COLS, 0::INPUT_CHANS, 0::OUTPUT_CHANS)
+        bias_sram load BIAS_DATA(0::OUTPUT_CHANS)        
+      }
       val rowspan = if (STRIDE.value == 1) INPUT_ROWS-(KERNEL_ROWS-STRIDE) else (INPUT_ROWS-(KERNEL_ROWS-STRIDE) >> 1)
       val colspan = if (STRIDE.value == 1) INPUT_COLS-(KERNEL_COLS-STRIDE) else (INPUT_COLS-(KERNEL_COLS-STRIDE) >> 1)
       Foreach(rowspan par P1){ R =>
@@ -84,25 +92,35 @@ object SingleLayerConv_RCIO extends SpatialApp {
         Foreach(colspan par P2){ C =>
           val col = C*2
           val local_data = SRAM[T](3,3,INPUT_CHANS_MAX)
-          val accum_line = SRAM.buffer[T](OUTPUT_CHANS_MAX)
-          Parallel{
-            local_data load INPUT_DATA(row::row+3, col::col+3, 0::INPUT_CHANS par loadPar)
-            accum_line load BIAS_DATA(row/STRIDE, col/STRIDE, 0::OUTPUT_CHANS par loadPar)
-          }
-          MemFold((0 until OUTPUT_CHANS par P5), accum_line)(INPUT_CHANS by 1 par P3){ ic =>
-            val local_accum_line = SRAM[T](OUTPUT_CHANS_MAX)
+          val accum_line_upcast = SRAM.buffer[T2](OUTPUT_CHANS_MAX)
+          val accum_line = SRAM[T](OUTPUT_CHANS_MAX)
+          local_data load INPUT_DATA(row::row+3, col::col+3, 0::INPUT_CHANS par loadPar)
+          MemReduce((0 until OUTPUT_CHANS par P5), accum_line_upcast)(INPUT_CHANS by 1 par P3){ ic =>
+            val local_accum_line = SRAM[T2](OUTPUT_CHANS_MAX)
             Foreach(OUTPUT_CHANS by 1 par P4){ oc =>
               val filter_elements = List.tabulate(3){i => List.tabulate(3){j => 
-                kernel_sram(i,j,ic,oc)
+                kernel_sram(i,j,ic,oc).to[T2]
               }}.flatten
               val data_elements = List.tabulate(3){i => List.tabulate(3){j => 
-                local_data(i,j,ic)
+                local_data(i,j,ic).to[T2]
               }}.flatten
               val accum = data_elements.zip(filter_elements).map{case(a,b) => a*b}.reduce{_+_}
               local_accum_line(oc) = accum
+              // if (debug) println(" at " + oc + "," + R + "," + C + " = " + filter_elements(0) + " * " + data_elements(0) + " + " +
+              //                     filter_elements(1) + " * " + data_elements(1) + " + " +
+              //                     filter_elements(2) + " * " + data_elements(2) + " + " +
+              //                     filter_elements(3) + " * " + data_elements(3) + " + " +
+              //                     filter_elements(4) + " * " + data_elements(4) + " + " +
+              //                     filter_elements(5) + " * " + data_elements(5) + " + " +
+              //                     filter_elements(6) + " * " + data_elements(6) + " + " +
+              //                     filter_elements(7) + " * " + data_elements(7) + " + " +
+              //                     filter_elements(8) + " * " + data_elements(8)
+              //                   )
             }
             local_accum_line
           }{_+_}
+          // RELU
+          Foreach(OUTPUT_CHANS by 1 par P6){i => accum_line(i) = max(0.to[T], accum_line_upcast(i).apply(27::12).as[T] + bias_sram(i))}
           OUTPUT_DATA(row/STRIDE,col/STRIDE,0::OUTPUT_CHANS par storePar) store accum_line
         }
       }
@@ -114,7 +132,7 @@ object SingleLayerConv_RCIO extends SpatialApp {
 
     // Compute Checks
     val gold = (0::(INPUT_ROWS-(KERNEL_ROWS-STRIDE)) / STRIDE, 0::(INPUT_COLS-(KERNEL_COLS-STRIDE)) / STRIDE, 0::OUTPUT_CHANS){(i,j,k) => 
-      Array.tabulate(INPUT_CHANS){page => 
+      val el = Array.tabulate(INPUT_CHANS){page => 
         if (debug && print_data) {
             println("Result working on " + k + "," + page + "," + i + "," + j)
             println(" Window: ")
@@ -130,10 +148,11 @@ object SingleLayerConv_RCIO extends SpatialApp {
         Array.tabulate(KERNEL_ROWS){ii => Array.tabulate(KERNEL_COLS){jj => 
           val pxl = input(i*STRIDE+ii,j*STRIDE+jj, page)
           val f = kernel(ii, jj, page, k)
-          if (debug && print_data) println(" Partial is " + pxl + " * " + f + " @ " + ii + "," + jj)
-          pxl * f
+          if (debug && print_data && f != 0.to[T]) println(" Partial is " + pxl + " * " + f + " @ " + ii + "," + jj)
+          pxl.to[T2] * f.to[T2]
         }}.flatten.reduce{_+_}
-      }.reduce{_+_} + bias(i, j, k)
+      }.reduce{_+_} >> 12
+      if (el.to[T] < 0.to[T]) 0.to[T] else {el.to[T] + bias(k)}
     }
   
     if (print_data) {
@@ -143,7 +162,7 @@ object SingleLayerConv_RCIO extends SpatialApp {
       printTensor3(results, "Extracted")  
 
       val bitmask = (0::(INPUT_ROWS-(KERNEL_ROWS-STRIDE)) / STRIDE, 0::(INPUT_COLS-(KERNEL_COLS-STRIDE)) / STRIDE, 0::OUTPUT_CHANS){(i,j,k) =>
-        if (results(i,j, k) == gold(i,j, k)) 1.to[Int] else 0.to[Int]
+        if (results(i,j, k) == gold(i,j,k)) 1.to[Int] else 0.to[Int]
       }
       val num_wrong = bitmask.length - bitmask.reduce{_+_}
       printTensor3(bitmask, "Matchup")
