4d8d29563248b3514f761fe116191ed65575f7f8
Matthew Feldman <mattfel@stanford.edu>
2018-04-02 14:58:59 -0700

    add mminner candidates

--- src/PLDI2018.scala
+++ src/PLDI2018.scala
@@ -706,6 +706,299 @@ object MatMult_inner7 extends SpatialApp { // Regression (Dense) // Args: 32 128
 
 }
 
+object MatMult_inner8 extends SpatialApp { // Regression (Dense) // Args: 32 128 128
+  override val target = AWS_F1
+
+
+  type X = FixPt[TRUE,_16,_16]
+
+  @virtualize
+  def MatMult_inner[T:Type:Num](A: Array[T], B: Array[T], mm: Int, nn: Int, pp: Int) = {
+    val M = ArgIn[Int]
+    val N = ArgIn[Int]
+    val P = ArgIn[Int]
+    setArg(M,mm)
+    setArg(N,nn)
+    setArg(P,pp)
+
+    val a = DRAM[T](M, P)
+    val b = DRAM[T](P, N)
+    val c = DRAM[T](M, N)
+
+    val bm = 64 (1 -> 1536)
+    val bn = 128 (64 -> 64 -> 1536)
+    val bp = 128 (64 -> 64 -> 1536)
+
+    val op = 2 (1 -> 6)
+    val mp = 2   (1 -> 64)
+    val ip = 16 (1 -> 64)
+    val P1 = 16
+    val P2 = 4
+    val loadPar = 16
+    val storePar = 16
+    val px = 1 (1 -> 1) // Cannot parallelize accum across k blocks
+
+    setMem(a, A)
+    setMem(b, B)
+
+    Accel {
+      Foreach(M by bm, N by bn par op){(i,j) =>
+        val tileC = SRAM[T](bm, bn)
+
+        MemReduce(tileC par P1)(P by bp par P2){k =>
+          val tileA = SRAM[T](bm, bp)
+          val tileB = SRAM[T](bp, bn)
+          val tileC_partial = SRAM[T](bm, bn)
+          Parallel {
+            tileA load a(i::i+bm, k::k+bp par loadPar) // Reads M*N*P times
+            tileB load b(k::k+bp, j::j+bn par loadPar)
+          }
+          Foreach(bm by 1, bn by 1 par mp){ (ii,jj) =>    // MetaPipe?
+            val prod = Reduce(Reg[T])(bp by 1 par ip){kk => tileA(ii, kk) * tileB(kk, jj) }{_+_}
+            tileC_partial(ii,jj) = prod.value // Is a unit pipe that should be recognized as accum
+          }
+          tileC_partial
+        }{_+_}
+        c(i::i+bm, j::j+bn par storePar) store tileC // Writes M*N times
+      }
+    }
+    getMem(c)
+  }
+
+  @virtualize
+  def main() = {
+    val M = args(0).to[Int]
+    val N = args(1).to[Int]
+    val P = args(2).to[Int]
+
+    val a = Array.tabulate(M){ i => Array.tabulate(P){ j => ((i*P + j)%8).to[X] } }
+    val b = Array.tabulate(P){ i => Array.tabulate(N){ j => ((i*N + j)%8).to[X] } }
+    // val a = Array.fill(M){ Array.fill(P){random[T](100)} }
+    // val b = Array.fill(P){ Array.fill(N){random[T](100)} }
+
+    val result = MatMult_inner(a.flatten, b.flatten, M, N, P)
+
+    val gold = Array.tabulate(M){i =>
+      val aRow = a(i)
+      Array.tabulate(N){j =>
+        val bCol = b.map{row => row(j)}
+        aRow.zip(bCol){_*_}.reduce{_+_}
+      }
+    }.flatten
+
+    val gold_cksum = gold.map(a => a).reduce{_+_}
+    val result_cksum = result.map(a => a).reduce{_+_}
+    printArray(gold, "Gold: ")
+    printArray(result, "Result: ")
+    println("expected cksum: " + gold_cksum)
+    println("result cksum:   " + result_cksum)
+
+    // (0 until M*N) foreach { i => assert(result(i) == gold(i)) }
+
+    val cksum = result_cksum == gold_cksum
+    println("PASS: " + cksum + " (MatMult_inner) * Remember to fix GEMM_MemoryHierarchy once issue #159 is fixed!")
+
+  }
+
+}
+
+object MatMult_inner9 extends SpatialApp { // Regression (Dense) // Args: 32 128 128
+  override val target = AWS_F1
+
+
+  type X = FixPt[TRUE,_16,_16]
+
+  @virtualize
+  def MatMult_inner[T:Type:Num](A: Array[T], B: Array[T], mm: Int, nn: Int, pp: Int) = {
+    val M = ArgIn[Int]
+    val N = ArgIn[Int]
+    val P = ArgIn[Int]
+    setArg(M,mm)
+    setArg(N,nn)
+    setArg(P,pp)
+
+    val a = DRAM[T](M, P)
+    val b = DRAM[T](P, N)
+    val c = DRAM[T](M, N)
+
+    val bm = 64 (1 -> 1536)
+    val bn = 128 (64 -> 64 -> 1536)
+    val bp = 128 (64 -> 64 -> 1536)
+
+    val op = 2 (1 -> 6)
+    val ump = 2
+    val mp = 2   (1 -> 64)
+    val ip = 16 (1 -> 64)
+    val P1 = 16
+    val P2 = 4
+    val loadPar = 16
+    val storePar = 16
+    val px = 1 (1 -> 1) // Cannot parallelize accum across k blocks
+
+    setMem(a, A)
+    setMem(b, B)
+
+    Accel {
+      Foreach(M by bm, N by bn par op){(i,j) =>
+        val tileC = SRAM[T](bm, bn)
+
+        MemReduce(tileC par P1)(P by bp par P2){k =>
+          val tileB = SRAM[T](bp, bn)
+          val tileC_partial = SRAM[T](bm, bn)
+          tileB load b(k::k+bp, j::j+bn par loadPar)
+          Foreach(bm by 1 par ump){ ii =>    // MetaPipe?
+            val tileA = SRAM[T](bp)
+            tileA load a(i+ii, k::k+bp par loadPar) // Reads M*N*P times
+            Foreach(bn by 1 par mp){jj => 
+              val prod = Reduce(Reg[T])(bp by 1 par ip){kk => tileA(kk) * tileB(kk, jj) }{_+_}
+              tileC_partial(ii,jj) = prod.value // Is a unit pipe that should be recognized as accum
+            }
+          }
+          tileC_partial
+        }{_+_}
+        c(i::i+bm, j::j+bn par storePar) store tileC // Writes M*N times
+      }
+    }
+    getMem(c)
+  }
+
+  @virtualize
+  def main() = {
+    val M = args(0).to[Int]
+    val N = args(1).to[Int]
+    val P = args(2).to[Int]
+
+    val a = Array.tabulate(M){ i => Array.tabulate(P){ j => ((i*P + j)%8).to[X] } }
+    val b = Array.tabulate(P){ i => Array.tabulate(N){ j => ((i*N + j)%8).to[X] } }
+    // val a = Array.fill(M){ Array.fill(P){random[T](100)} }
+    // val b = Array.fill(P){ Array.fill(N){random[T](100)} }
+
+    val result = MatMult_inner(a.flatten, b.flatten, M, N, P)
+
+    val gold = Array.tabulate(M){i =>
+      val aRow = a(i)
+      Array.tabulate(N){j =>
+        val bCol = b.map{row => row(j)}
+        aRow.zip(bCol){_*_}.reduce{_+_}
+      }
+    }.flatten
+
+    val gold_cksum = gold.map(a => a).reduce{_+_}
+    val result_cksum = result.map(a => a).reduce{_+_}
+    printArray(gold, "Gold: ")
+    printArray(result, "Result: ")
+    println("expected cksum: " + gold_cksum)
+    println("result cksum:   " + result_cksum)
+
+    // (0 until M*N) foreach { i => assert(result(i) == gold(i)) }
+
+    val cksum = result_cksum == gold_cksum
+    println("PASS: " + cksum + " (MatMult_inner) * Remember to fix GEMM_MemoryHierarchy once issue #159 is fixed!")
+
+  }
+
+}
+
+object MatMult_inner10 extends SpatialApp { // Regression (Dense) // Args: 32 128 128
+  override val target = AWS_F1
+
+
+  type X = FixPt[TRUE,_16,_16]
+
+  @virtualize
+  def MatMult_inner[T:Type:Num](A: Array[T], B: Array[T], mm: Int, nn: Int, pp: Int) = {
+    val M = ArgIn[Int]
+    val N = ArgIn[Int]
+    val P = ArgIn[Int]
+    setArg(M,mm)
+    setArg(N,nn)
+    setArg(P,pp)
+
+    val a = DRAM[T](M, P)
+    val b = DRAM[T](P, N)
+    val c = DRAM[T](M, N)
+
+    val bm = 64 (1 -> 1536)
+    val bn = 128 (64 -> 64 -> 1536)
+    val bp = 128 (64 -> 64 -> 1536)
+
+    val op = 2 (1 -> 6)
+    val ump = 2
+    val mp = 2   (1 -> 64)
+    val ip = 16 (1 -> 64)
+    val P1 = 16
+    val P2 = 4
+    val loadPar = 16
+    val storePar = 16
+    val px = 1 (1 -> 1) // Cannot parallelize accum across k blocks
+
+    setMem(a, A)
+    setMem(b, B)
+
+    Accel {
+      Foreach(M by bm, N by bn par op){(i,j) =>
+        val tileC = SRAM[T](bm, bn)
+
+        MemReduce(tileC par P1)(P by bp par P2){k =>
+          val tileB = SRAM[T](bp, bn)
+          val tileC_partial = SRAM[T](bm, bn)
+          tileB load b(k::k+bp, j::j+bn par loadPar)
+          Foreach(bm by 1 par ump){ ii =>    // MetaPipe?
+            val tileA = SRAM[T](bp)
+            tileA load a(i+ii, k::k+bp par loadPar) // Reads M*N*P times
+            Foreach(bn by 1 par mp){jj => 
+              val prod = Reduce(Reg[T])(bp by 1 par ip){kk => 
+                val tileB = SRAM[T](bn)
+                tileB load b(k+kk, j::j+bn par loadPar)
+                tileA(kk) * tileB(jj) 
+              }{_+_}
+              tileC_partial(ii,jj) = prod.value // Is a unit pipe that should be recognized as accum
+            }
+          }
+          tileC_partial
+        }{_+_}
+        c(i::i+bm, j::j+bn par storePar) store tileC // Writes M*N times
+      }
+    }
+    getMem(c)
+  }
+
+  @virtualize
+  def main() = {
+    val M = args(0).to[Int]
+    val N = args(1).to[Int]
+    val P = args(2).to[Int]
+
+    val a = Array.tabulate(M){ i => Array.tabulate(P){ j => ((i*P + j)%8).to[X] } }
+    val b = Array.tabulate(P){ i => Array.tabulate(N){ j => ((i*N + j)%8).to[X] } }
+    // val a = Array.fill(M){ Array.fill(P){random[T](100)} }
+    // val b = Array.fill(P){ Array.fill(N){random[T](100)} }
+
+    val result = MatMult_inner(a.flatten, b.flatten, M, N, P)
+
+    val gold = Array.tabulate(M){i =>
+      val aRow = a(i)
+      Array.tabulate(N){j =>
+        val bCol = b.map{row => row(j)}
+        aRow.zip(bCol){_*_}.reduce{_+_}
+      }
+    }.flatten
+
+    val gold_cksum = gold.map(a => a).reduce{_+_}
+    val result_cksum = result.map(a => a).reduce{_+_}
+    printArray(gold, "Gold: ")
+    printArray(result, "Result: ")
+    println("expected cksum: " + gold_cksum)
+    println("result cksum:   " + result_cksum)
+
+    // (0 until M*N) foreach { i => assert(result(i) == gold(i)) }
+
+    val cksum = result_cksum == gold_cksum
+    println("PASS: " + cksum + " (MatMult_inner) * Remember to fix GEMM_MemoryHierarchy once issue #159 is fixed!")
+
+  }
+
+}
 
 object MatMult_outer1 extends SpatialApp { // Regression (Dense) // Args: 32 128 128
   override val target = AWS_F1
