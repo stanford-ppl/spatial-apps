fafb7f7c4a912be5fb9e4447cad39085893dd072
Yaqi Zhang <hsbzyq@gmail.com>
2018-08-05 10:28:33 -0700

    update experiment script

--- bin/best_results.py
+++ bin/best_results.py
@@ -40,17 +40,17 @@ bestapps += ['lenet_loops__batch_par_1_conv1_par_2_conv2_par_4_mat1_par_2_mat2_p
 bestapps += ['SGD_minibatch__D_64_N_16384_E_2_ts_1024_mp1_8_mp2_16']
 
 gen_pir()
-psim_generic("psim_p2p_ideal" , ["gen_pir"]  , "--net=p2p --vlink=0 --slink=0 --fifo-depth=20 --vfifo=4 --proute-algo=route_dor_YX --link-prop=db --flit-width=512")
-psim_generic("psim_p2p"       , ["psim_p2p_ideal"]  , "--net=p2p --vlink=0 --slink=0 --fifo-depth=4  --vfifo=4 --proute-algo=route_dor_YX --link-prop=db --flit-width=512" )
-psim_generic("psim_v3_s4"     , ["psim_p2p_ideal"] , "--net=static --vlink=3 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_dor_YX --link-prop=db --flit-width=512" )
-psim_generic("psim_v2_s4"     , ["psim_p2p_ideal"] , "--net=static --vlink=2 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_dor_YX --link-prop=db --flit-width=512" )
-psim_generic("psim_v2_s4_cd"  , ["psim_p2p_ideal"] , "--net=static --vlink=2 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_dor_YX --link-prop=cd --flit-width=512" )
-psim_generic("psim_D_v1_s4"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=1 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_dor_YX --link-prop=db --flit-width=512" )
-psim_generic("psim_D_v1_s4_f32"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=1 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_dor_YX --link-prop=db --flit-width=32" )
-psim_generic("psim_D_v1_s4_val"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=1 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_min_directed_valient --link-prop=db --flit-width=512" )
-psim_generic("psim_D_v2_s4"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=2 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_dor_YX --link-prop=db --flit-width=512" )
-psim_generic("psim_D_v0_s0"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=0 --slink=0 --fifo-depth=4 --vfifo=4 --proute-algo=route_dor_YX --link-prop=db --flit-width=512" )
-psim_generic("psim_D_v0_s4"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=0 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_dor_YX --link-prop=db --flit-width=512" )
+psim_generic("psim_p2p_ideal" , ["gen_pir"]  , "--net=p2p --vlink=0 --slink=0 --fifo-depth=20 --vfifo=4 --link-prop=db --flit-width=512")
+psim_generic("psim_p2p"       , ["psim_p2p_ideal"]  , "--net=p2p --vlink=0 --slink=0 --fifo-depth=4  --vfifo=4 --link-prop=db --flit-width=512" )
+psim_generic("psim_v3_s4"     , ["psim_p2p_ideal"] , "--net=static --vlink=3 --slink=4 --fifo-depth=4 --vfifo=4 --link-prop=db --flit-width=512" )
+psim_generic("psim_v2_s4"     , ["psim_p2p_ideal"] , "--net=static --vlink=2 --slink=4 --fifo-depth=4 --vfifo=4 --link-prop=db --flit-width=512" )
+psim_generic("psim_v2_s4_cd"  , ["psim_p2p_ideal"] , "--net=static --vlink=2 --slink=4 --fifo-depth=4 --vfifo=4 --link-prop=cd --flit-width=512" )
+
+psim_generic("psim_D_v1_s4"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=1 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_min_directed_valient --link-prop=db --flit-width=512" )
+psim_generic("psim_D_v0_s0"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=0 --slink=0 --fifo-depth=4 --vfifo=4 --proute-algo=route_min_directed_valient --link-prop=db --flit-width=512" )
+# psim_generic("psim_D_v1_s4_f256"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=1 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_min_directed_valient --link-prop=db --flit-width=256" )
+# psim_generic("psim_D_v2_s4"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=2 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_min_directed_valient --link-prop=db --flit-width=512" )
+# psim_generic("psim_D_v0_s4"   , ["psim_p2p_ideal"] , "--net=dynamic --vlink=0 --slink=4 --fifo-depth=4 --vfifo=4 --proute-algo=route_min_directed_valient --link-prop=db --flit-width=512" )
 link_count()
 
 def runbest():
